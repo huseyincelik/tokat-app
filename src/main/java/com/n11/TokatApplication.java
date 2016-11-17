2440937387da8134b28035bb0b2641e6f40a0eae
huseyincelik <huseyin.celik@mac111239.local>
2016-11-17 10:30:08 +0300

    initial project files

---
+++ src/main/java/com/n11/TokatApplication.java
@@ -0,0 +1,12 @@
+package com.n11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokatApplication.class, args);
+	}
+}

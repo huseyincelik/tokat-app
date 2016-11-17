d498159fd73db25db1c52053834d56eccc217326
huseyincelik <huseyin.celik@mac111239.local>
2016-11-17 11:16:07 +0300

    basic form

--- src/main/java/com/n11/controller/GreetController.java
+++ src/main/java/com/n11/controller/GreetController.java
@@ -1,17 +1,25 @@
 package com.n11.controller;
 
+import com.n11.model.Greeting;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetController {
 
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
+    @GetMapping("/greeting")
+    public String greeting(Model model) {
+        model.addAttribute("greeting", new Greeting());
         return "greeting";
+    }
+
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting) {
+        return "result";
     }
 
 }
